5712e1c9b3f297cf7a1725623c1fddf2203b2a0d
SvetlanaPivovarova <june_sv@mail.ru>
2023-01-23 00:01:03 +0600

    add solution for task 10,13,15

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, что третьей цифры нет.
+//System.Random numSintezator = new System.Random();
+char[] digits = Console.ReadLine().ToCharArray();
+  
+ if (digits.Length >= 3)
+   {
+    Console.WriteLine("третья цифра: " + digits[2]);
+   }
+    else
+   {
+   Console.WriteLine("третьей цифры нет");
+}
+
