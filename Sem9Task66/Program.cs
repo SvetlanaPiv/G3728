487e78bb356f1a9a6f1db318fc0698c49e92a882
SvetlanaPivovarova <june_sv@mail.ru>
2023-02-12 23:10:02 +0600

    add solution for task 63,64,65,66,67,68,69

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int m = ReadData("Введите число M: ");
+int n = ReadData("Введите число N: ");
+
+SumFromMToN(m, n);
+
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.Write("сумма: " + SumMN(m - 1, n));
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int m = ReadData("Введите число m: ");
+int n = ReadData("Введите число n: ");
+
+AkkermanFunction(m,n);
+
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write("вычисление функции: " + Akkerman(m, n)); 
+}
+
+// расчет функции Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
